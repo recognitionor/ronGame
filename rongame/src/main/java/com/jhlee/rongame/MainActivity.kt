a4b0c9a4f1c86c11b8b35a678f7c8b2fbe1ff097
junghee <junghee@nhn.com>
2023-06-30 14:35:53 +0900

    코드는 없지만 참조 관계는 참조 완료

---
+++ rongame/src/main/java/com/jhlee/rongame/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jhlee.rongame
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
