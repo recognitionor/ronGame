057bdee25237fc757079086fd130c37fc9077838
junghee <junghee@nhn.com>
2023-08-08 10:55:24 +0900

    code clean

--- rongame/src/main/java/com/jhlee/rongame/presentation/etc/EtcViewModel.kt
+++ rongame/src/main/java/com/jhlee/rongame/presentation/etc/EtcViewModel.kt
@@ -1,6 +1,5 @@
 package com.jhlee.rongame.presentation.etc
 
-import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
